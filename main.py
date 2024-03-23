f7b519bdca4a7a1be4b4ebeb574abad8f291b67d
Richard Lucas <webmaster@applebiter.com>
2024-03-23 16:25:12 -0400

    Updated the main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from noveler.noveler import Noveler
 
 """Example database connection strings
 
-PostgreSQL = Core("postgresql+psycopg://{dbuser}:{dbpassword}@{dbhost}:{dbport}/{dbname}", echo=False)
+PostgreSQL = Noveler("postgresql+psycopg://{dbuser}:{dbpassword}@{dbhost}:{dbport}/{dbname}", echo=False)
     Shown here using the psycopg driver, also known as psycopg3
-MySQL = Core("mysql+mysqlconnector://{dbuser}:{dbpassword}@{dbhost}:{dbport}/{dbname}", echo=False)
+MySQL = Noveler("mysql+mysqlconnector://{dbuser}:{dbpassword}@{dbhost}:{dbport}/{dbname}", echo=False)
     Shown here using the mysql-connector-python driver
-SQLite = Core("sqlite:///{dbname}.db", echo=False)
+SQLite = Noveler("sqlite:///{dbname}.db", echo=False)
 
-You supply the connection string to the Application class, and it will automatically generate the database schema. Shown 
+You supply the connection string to the Noveler class, and it will automatically generate the database schema. Shown 
 here using the sqlite3 driver, which is included in the Python standard library. The echo parameter is optional and
 defaults to False. If set to True, it will print all SQL commands to the console. This is useful for debugging.
 """
