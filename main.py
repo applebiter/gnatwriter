ae403ec6f0d710f0d4c7a0b0f69c106c7b3f5b00
Richard Lucas <webmaster@applebiter.com>
2024-03-25 17:35:50 -0400

    Hail Eris

--- main.py
+++ main.py
@@ -15,26 +15,6 @@ defaults to False. If set to True, it will print all SQL commands to the console
 
 noveler = Noveler("sqlite:///noveler/noveler.db")
 
-# Scene 1: Introduction:
-content = """Noveler is a writing tool that helps the author manage story structure, and also separately develop 
-characters, locations, and events. It's built ready to accommodate a universe, rather than just one story. Noveler is 
-a framework for developing an API or a desktop application with a robust SQL back end. The framework itself can be used 
-for a a single-user application or a multi-user application, with core user management features built in.
-
-The framework is minimal but solid, and it is all it can be without a friendly user interface, and a paradigm choice 
-to guide project file structure and state structure. I have been unable to settle on a paradigm, and have considered the 
-following candidates:
-
-1. A GUI front-end like PySide6 or PyQt6, which would be a lot of work, but would be the most user-friendly.
-2. A REST API using Flask, which would be a lot of work, and still wouldn't provide a front end.
-3. A command-line interface, which would be the least work, but would be the least user-friendly.
-
-I have decided to go with both a GUI front end and a REST API, each separate but interoperable projects. The GUI front
-end will be a PySide6 application, and the REST API will be a Flask application. So, two more git repositories to 
-manage.
-"""
-scene_1 = noveler("scene").get_scene_by_id
-
 if noveler("story").has_stories():
     print("Stories found:")
     print(" ")
@@ -47,6 +27,7 @@ if noveler("story").has_stories():
                 if noveler("chapter").has_scenes(chapter.id):
                     for scene in noveler("scene").get_scenes_by_chapter_id(chapter.id):
                         print(f"\t\t\t\t{scene.title}  # Scene ID# {scene.id}")
+                        # print(f"\t\t\t\t\t{scene.content}")
 
 else:
     print("No stories found")
